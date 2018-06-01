34a496b3c5a85bcc14b6d0b360e8eb9d0eeaba21
Brian Fennell <brian.f.fennell@gmail.com>
2018-05-31 20:33:28 -0400

    accordion

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 
-// survey form submitted alert
-
-
-function formsubmitted() {
-  alert("Thank you for your feedback!");
-}
-
 //display input box
 
-var inputBox = document.getElementById('free form');
-
-inputBox.onkeyup = function() {
-  document.getElementById('printchatbox').innerHTML = inputBox.value;
+function formsubmitted() {
+    document.getElementById('display').innerHTML =
+                document.getElementById('free form').value;
 }
+
+
+//accordion
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
 
 
 
